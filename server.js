4a46f120218cb9f172d03ca971dfcb7e934fa9ba
musjawai <mustafajawaid@berkeley.edu>
2024-07-08 21:19:36 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// console.log("Hello World!");
+// // console.log(global);
+
+const os = require("os");
+const path = require("path");
+const { add, subtract, multiply, divide } = require("./math");
+
+console.log(add(5, 5));
+console.log(subtract(5, 5));
+console.log(multiply(5, 5));
+console.log(divide(5, 5));
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
