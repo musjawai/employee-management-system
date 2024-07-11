62f505d31a4e312752c28e7b990ee83ab9ecf4b2
musjawai <mustafajawaid@berkeley.edu>
2024-07-11 16:08:32 -0700

    routers

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
@@ -14,7 +14,7 @@ const logEvents = async (message) => {
       fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, "logs", "..", "eventlog.txt"),
+      path.join(__dirname, "..", "logs", logName),
       logItem
     );
   } catch (err) {
@@ -22,4 +22,10 @@ const logEvents = async (message) => {
   }
 };
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logEvents, logger };
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const path = require("path");
-const logEvents = require("./middleware/logEvents");
+const { logEvents, logger } = require("./middleware/logEvents");
+const { errorHandler } = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
-app.use((req, res, next) => {
-  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
+app.use(logger);
+
+const whitelist = [
+  "http://localhost:3500",
+  "https://www.google.com",
+  "http://127.0.0.1:5500",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -29,9 +44,18 @@ app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "Not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
