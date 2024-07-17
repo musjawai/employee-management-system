ba73a163dcfd9c2156794ff2a28738ae7013a122
musjawai <mustafajawaid@berkeley.edu>
2024-07-17 12:01:58 -0700

    auth service

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,4 +6,21 @@ const usersDB = {
 };
 const bcrypt = require("bcrypt");
 
-const handleLogin = async;
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "username and password are requried" });
+  const userInDB = usersDB.users.find((person) => person.username === user);
+  if (!userInDB) return res.sendStatus(401);
+  const match = await bcrypt.compare(pwd, userInDB.password);
+  if (match) {
+    // create JWT
+    res.json({ success: `User ${user} is logged in!` });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
